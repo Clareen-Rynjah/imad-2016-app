afc9a952bc297c747aa1ded156f4a367fb7d77c3
Clareen-Rynjah <clareenjrynnz@gmail.com>
2016-10-01 01:52:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -72,7 +72,8 @@ var htmlTemplate = `<html>
               </p>
           </div>
       </div>
-</html>`;
+</html>
+`;
 return htmlTemplate;
 }
 app.get('/', function (req, res) {
@@ -90,6 +91,10 @@ app.get('/ui/style.css', function (req, res)
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/main.js', function (req, res)
+{
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
