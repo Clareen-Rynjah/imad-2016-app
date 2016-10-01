9ecec665397188eb733d015eabb74439c5d3f44c
Clareen-Rynjah <clareenjrynnz@gmail.com>
2016-10-01 23:13:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,12 @@ var element =document.getElementbyId('main-text');
 element.InnerHTML='New value';
 
 var img=document.getElementbyId('modi');
+marinLeft=0;
+function moveRight()
+{
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft+ 'px';
+}
 img.onclick=function(){
     var interval=setInterval(moveRight,100);
 };
